ff69075cfcebe69c0493650e3b2b2944d405190c
Yi Hang <yihang19990726@gmail.com>
2026-04-24 10:59:33 +0800

    homepage redesign

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: use_build_context_synchronously
-
+// ignore_for_file: use_build_context_synchronously, deprecated_member_use
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:quick_clean/models/service.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:animate_do/animate_do.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key}); 
@@ -15,11 +16,54 @@ class _HomeScreenState extends State<HomeScreen> {
   List<dynamic> users = [];
   bool isLoading = true;
 
+  // Sample data for services and workers (Replace with real data from Supabase in production)
+  List<Service> services = [
+    Service('Cleaning', 'https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/2x/external-cleaning-labour-day-vitaliy-gorbachev-flat-vitaly-gorbachev.png'),
+    Service('Plumber', 'https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/2x/external-plumber-labour-day-vitaliy-gorbachev-flat-vitaly-gorbachev.png'),
+    Service('Electrician', 'https://img.icons8.com/external-wanicon-flat-wanicon/2x/external-multimeter-car-service-wanicon-flat-wanicon.png'),
+    Service('Painter', 'https://img.icons8.com/external-itim2101-flat-itim2101/2x/external-painter-male-occupation-avatar-itim2101-flat-itim2101.png'),
+    Service('Carpenter', 'https://img.icons8.com/fluency/2x/drill.png'),
+    Service('Gardener', 'https://img.icons8.com/external-itim2101-flat-itim2101/2x/external-gardener-male-occupation-avatar-itim2101-flat-itim2101.png'),
+  ];
+
+  List<dynamic> workers = [
+    ['Alfredo Schafer', 'Plumber', 'https://images.unsplash.com/photo-1506803682981-6e718a9dd3ee?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=c3a31eeb7efb4d533647e3cad1de9257', 4.8],
+    ['Michelle Baldwin', 'Cleaner', 'https://uifaces.co/our-content/donated/oLkb60i_.jpg', 4.6],
+    ['Brenon Kalu', 'Driver', 'https://uifaces.co/our-content/donated/VUMBKh1U.jpg', 4.4]
+  ];
+  
+
   @override 
   void initState() { 
     super.initState(); 
+    fetchUsers();
   } 
   
+  Future<void> fetchUsers() async {
+    setState(() => isLoading = true);
+    final user = Supabase.instance.client.auth.currentUser;
+    if (user == null) {
+      setState(() => isLoading = false);
+      return;
+    }
+
+    try {
+      final data = await Supabase.instance.client
+          .from('users')
+          .select();
+
+      setState(() {
+        users = data;
+        isLoading = false;
+      });
+
+      //print('Fetched users: $users');
+
+    } catch (e) {
+      //print('Error fetching users: $e');
+    }
+  }
+
   Future<void> _logout(BuildContext context) async {
     try {
       // 1. Sign out from Supabase
@@ -46,30 +90,126 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('QuickClean', style: TextStyle(fontWeight: FontWeight.bold)),
         actions: [IconButton(icon: Icon(Icons.notifications), onPressed: () {})],
       ),
-      body: Padding(
-            padding: const EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Hello! Which service do you need?', 
-                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-            SizedBox(height: 20),
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
+            FadeInUp(child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade200,
+                      offset: Offset(0, 4),
+                      blurRadius: 10.0,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: Image.network('https://images.pexels.com/photos/355164/pexels-photo-355164.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb', width: 70,)
+                        ),
+                        SizedBox(width: 15,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(users[0]['name'] ?? '-', 
+                              style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 5,),
+                            Text(users[0]['email'] ?? '-', 
+                              style: TextStyle(color: Colors.black.withOpacity(0.7), fontSize: 18),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 20,),
+                    // Container(
+                    //   height: 50,
+                    //   decoration: BoxDecoration(
+                    //     color: Colors.blue,
+                    //     borderRadius: BorderRadius.circular(15.0)
+                    //   ),
+                    //   child: Center(child: Text('View Profile', style: TextStyle(color: Colors.white, fontSize: 18),)),
+                    // )
+                  ],
+                ),
+              ),
+            )),
+            SizedBox(height: 20,),
+            FadeInUp(child: Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  _serviceCard('Home Cleaning', Icons.home, Colors.blue),
-                  _serviceCard('Office Cleaning', Icons.business, Colors.green),
-                  _serviceCard('Window Wash', Icons.wb_sunny, Colors.orange),
-                  _serviceCard('Carpet Steam', Icons.layers, Colors.purple),
+                  Text('Categories', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                  // TextButton(
+                  //   onPressed: () {}, 
+                  //   child: Text('View all',)
+                  // )
                 ],
               ),
+            )),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              height: 300,
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                ),
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: services.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return FadeInUp(
+                    delay: Duration(milliseconds: 500 * index),
+                    child: serviceContainer(services[index].imageURL, services[index].name, index));
+                }
+              ),
             ),
-          ],
-        ),
-      ), 
+            FadeInUp(child: Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Top Rated', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                  // TextButton(
+                  //   onPressed: () {}, 
+                  //   child: Text('View all',)
+                  // )
+                ],
+              ),
+            )),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              height: 120,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: workers.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return FadeInUp(
+                    delay: Duration(milliseconds: 500 * index),
+                    child: workerContainer(workers[index][0], workers[index][1], workers[index][2], workers[index][3]));
+                }
+              ),
+            ),
+            SizedBox(height: 150,),
+          ]
+        )
+      ),
       // Using BottomAppBar instead of BottomNavigationBar for custom layouts
       bottomNavigationBar: BottomAppBar(
         shape: CircularNotchedRectangle(),
@@ -99,17 +239,76 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _serviceCard(String title, IconData icon, Color color) {
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(icon, size: 40, color: color),
-          SizedBox(height: 10),
-          Text(title, style: TextStyle(fontWeight: FontWeight.w600)),
-        ],
+  serviceContainer(String image, String name, int index) {
+    return GestureDetector(
+      child: Container(
+        margin: EdgeInsets.only(right: 10),
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: Colors.grey.shade100,
+          border: Border.all(
+            color: Colors.blue.withOpacity(0),
+            width: 2.0,
+          ),
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network(image, height: 45),
+            SizedBox(height: 20,),
+            Text(name, style: TextStyle(fontSize: 15),)
+          ]
+        ),
+      ),
+    );
+  }
+
+  workerContainer(String name, String job, String image, double rating) {
+    return GestureDetector(
+      child: AspectRatio(
+        aspectRatio: 3.5,
+        child: Container(
+          margin: EdgeInsets.only(right: 20),
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.grey.shade200,
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                child: Image.network(image)
+              ),
+              SizedBox(width: 20,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(name, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 5,),
+                  Text(job, style: TextStyle(fontSize: 15),)
+                ],
+              ),
+              Spacer(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(rating.toString(), style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 5,),
+                  Icon(Icons.star, color: Colors.orange, size: 20,)
+                ],
+              )
+            ]
+          ),
+        ),
       ),
     );
   }
